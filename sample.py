79b788057ee30f22a5e24af352e3d9307e8437f7
n-katou <102382177+n-katou@users.noreply.github.com>
2022-10-09 00:54:37 +0900

    10/9

--- sample.py
+++ sample.py
@@ -1,1 +1,15 @@
-print('Hello')     
+number = 1
+number2 = 1.0
+greeting = 'こんにちは'
+is_ok = True
+
+print(number, type(number))
+print(number2, type(number2))
+print(greeting, type(greeting))
+print(is_ok, type(is_ok))
+
+print(2 > 1)
+print(2 < 1)
+
+print(True)
+print('テストの点数は' + str(100) + '点です')
